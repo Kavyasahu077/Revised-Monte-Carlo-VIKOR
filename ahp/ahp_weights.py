68d417db2b940354e18e6f655ec602d8e31d8528
Kavyasahu077 <kavyasahu.0712@gmail.com>
2026-06-02 18:40:18 +0530

    Refactor ahp_weights function to return fixed weights

--- ahp/ahp_weights.py
+++ ahp/ahp_weights.py
@@ -3,17 +3,8 @@
 import numpy as np
 
 def ahp_weights():
-    a12 = np.random.triangular(2, 3, 4)
-    a13 = np.random.triangular(3, 4, 5)
-    a23 = np.random.triangular(2, 3, 4)
-
-    A = np.array([
-        [1, a12, a13],
-        [1/a12, 1, a23],
-        [1/a13, 1/a23, 1]
+    return np.array([
+        0.50,  # Failure Probability
+        0.35,  # Severity
+        0.15   # Non Detectability
     ])
-
-    eigvals, eigvecs = np.linalg.eig(A)
-    w = np.real(eigvecs[:, np.argmax(eigvals)])
-
-    return w / np.sum(w)
